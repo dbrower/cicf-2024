896c91830798f0005515b429be97f0628980d5b0
Don Brower <dbrower@nd.edu>
2023-02-28 16:42:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Week 4
+======
+
+This repository is for the technical track of the CI Compass Fellowship program.
+
---
+++ fib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def fib(n):
+    a, b = 1, 1
+    while n > 1:
+        a, b = b, a+b
+        n -= 1
+    return a
+
+print([fib(x) for x in range(1,10)])
+    
